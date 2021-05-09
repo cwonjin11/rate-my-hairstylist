bd3de4933b6cb016f2d30e21e53eadb11d42ac92
Wonjin Cho <cwonjin11@gmail.com>
2021-05-09 01:09:54 -0400

    helper method added and applied to layout/_nav partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+    
+    helper_method :logged_in?, :current_user
+    #any method we want access through out entire application
+
+    def current_user
+        @user = User.find_by_id(session[:user_id]) 
+        #or 
+        # @user ||= User.find_by_id(session[:user_id]) 
+        # if the user is @user ||= (or, if there's no user set user as) User.find_by_id(session[:user_id])
+    end
+
+
+    def logged_in?
+        !current_user.nil? #.nil means "is this nil?"
+        #  current_user != nil
+        
+        ##    !    <<<<{Will Be Negated}<<<<    (  (current_user.nil?) = TRUE  )
+
+    end 
+
+
 end
