d05848c7b2b371851c2fc73af48e7c8126225baa
Wonjin Cho <cwonjin11@gmail.com>
2021-05-09 01:13:36 -0400

    get routes commented out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,50 @@
 Rails.application.routes.draw do
+  # get 'stylists/index'
+  # get 'stylists/show'
+  # get 'stylists/new'
+  # get 'stylists/create'
+  # get 'stylists/edit'
+  # get 'stylists/update'
+  # get 'stylists/destroy'
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/login'
+  # get 'sessions/destroy'
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
+########   SESSIONS ROUTES  ### SIGN UP IS IN USER ROUTES ALREADY!! 
+
+get 'login', to: "sessions#new"
+
+post 'login', to: "sessions#create"
+
+delete 'logout', to: "sessions#destroy"
+
+
+# 🚧 # THINGS WE HAVE TO BUILD OUT
+
+  get 'welcome', to: "session#welcome" 
+
+# 🚧 #
+
+######## 
+# REMEMBER FOLKS  ::  NO resource :sessions 
+
+
+
   resources :users do
+
     resources :user_stylists
+    
   end
+
   resources :user_stylists
   resources :stylists
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
+
+
 end
