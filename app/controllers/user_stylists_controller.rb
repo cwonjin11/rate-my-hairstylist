02eed5a91a37b21d5202ddf64f78e6d7716302e0
Wonjin Cho <cwonjin11@gmail.com>
2021-05-10 12:27:51 -0400

    show routes defined with stylist.name

--- app/controllers/user_stylists_controller.rb
+++ app/controllers/user_stylists_controller.rb
@@ -1,12 +1,44 @@
 class UserStylistsController < ApplicationController
-
+    before_action :find_user_stylist,  only: [:show]
 
     def index 
         @user_stylists = current_user.user_stylists
+
         # binding.pry
+        # params[:stylist_id_on_user_stylist] = @user_stylist.stylist_id
+        # @stylist = Stylist.find_by_id(params[:stylist_id_on_user_stylist])
+        # binding.pry
+
     end
     def show
-        # have access to @user_hobby
-        @user_stylist = UserStylist.find_by_id(params[:id])
+        # @user_stylist = UserStylist.find_by_id(params[:id])   <==before action! go!
+
+        params[:stylist_id_on_user_stylist] = @user_stylist.stylist_id
+        @stylist = Stylist.find_by_id(params[:stylist_id_on_user_stylist])
+        # binding.pry
+
     end
+
+
+    def new
+        @user_hobby = UserHobby.new
+    end
+    def create 
+
+    end
+
+
+
+    private
+
+
+    def find_user_stylist
+        @user_stylist= UserStylist.find_by_id(params[:id])
+    end
+
+    def user_stylist_params
+        params.require(:user_stylist).permit!
+    end 
+
+
 end
