58e392a1a1de08421d96d6c2b9424959c5bf0f02
Wonjin Cho <cwonjin11@gmail.com>
2021-05-11 12:38:13 -0400

    minor changes

--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -9,11 +9,37 @@ class StylistsController < ApplicationController
   end
 
 
-  
+
   def new
+      @stylist = Stylist.new
   end
   def create
+      @stylist = @user.stylists.build(stylist_params)
+      binding.pry
+        
+    if @stylist.save
+      binding.pry
+        redirect_to new_user_user_stylist_path(@user)
+    else
+        render :new
+    end
+
   end
+
+#   @wine = Wine.find_or_create_by(wine_params)
+#   @wine.creator_id = current_user.id
+#  if @wine.valid?
+#    @wine.name = @wine.full_name
+#    @wine.save
+#    redirect_to @wine
+#  else
+#    render :new
+#  end
+
+
+
+
+
 
   def edit
   end
@@ -23,4 +49,15 @@ class StylistsController < ApplicationController
 
   def destroy
   end
+end
+
+
+
+
+private 
+
+def stylist_params
+  #in our params, we wanna require who? (stylist!), and permit what? (attributes)
+  params.require(:stylist).permit(:name, :phone, :shop_name, :address) 
+
 end
