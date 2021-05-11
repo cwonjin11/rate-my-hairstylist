f06d3f2d09b9c2a575f132ded263a2fd8378ef2d
Wonjin Cho <cwonjin11@gmail.com>
2021-05-11 12:40:27 -0400

    scope method : average_rating defined and added to index show

--- app/models/stylist.rb
+++ app/models/stylist.rb
@@ -2,4 +2,22 @@ class Stylist < ApplicationRecord
     has_many :user_stylists
     has_many :users, through: :user_stylists
 
+   
+
+    # def categories_attributes=(category_attributes)
+    #   category_attributes.values.each do |category_attribute|
+    #     if category_attribute["name"].present?
+    #       category = Category.find_or_create_by(category_attribute)
+    #       self.categories << category
+    #     end
+    #   end
+    # end
+
+
+    #### scope method ####
+    #hair stylist average rating#
+    def average_rating
+        UserStylist.where(stylist_id: self.id).average(:stars).to_i
+      end
+
 end
