f543f3340adad1f383ef4c137ee7fe0b5935d246
Wonjin Cho <cwonjin11@gmail.com>
2021-05-12 00:20:13 -0400

    username, email validation presence, uniqueness appended

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
     has_many :stylists, through: :user_stylists
 
     has_secure_password
-    
+
+    validates_presence_of :username, :email
+    validates_uniqueness_of :username, :email 
 
 end
