ac2732df510f269394a866d99f04b57996e849c4
Wonjin Cho <cwonjin11@gmail.com>
2021-05-13 14:09:51 -0400

    scope method defined

--- app/models/stylist.rb
+++ app/models/stylist.rb
@@ -3,21 +3,51 @@ class Stylist < ApplicationRecord
     has_many :users, through: :user_stylists
 
    
+    validates_presence_of :name, :phone, :shop_name, :address
+    validates_uniqueness_of :name, :phone
 
-    # def categories_attributes=(category_attributes)
-    #   category_attributes.values.each do |category_attribute|
-    #     if category_attribute["name"].present?
-    #       category = Category.find_or_create_by(category_attribute)
-    #       self.categories << category
-    #     end
-    #   end
-    # end
-
+    
 
     #### scope method ####
     #hair stylist average rating#
     def average_rating
-        UserStylist.where(stylist_id: self.id).average(:stars).to_i
-      end
+        UserStylist.where(stylist_id: self.id).average(:stars).to_f.round(1)
+    end
+
+    def hairstylist_rating
+      UserStylist.find_by(stylist_id: self.id).stars
+    end
+
+
+    scope :most_reviews, -> {(
+      select("stylists.name, stylists.id, count(user_stylists.id) as user_stylists_count")
+      .joins(:user_stylists)
+      .group("stylists.id")
+      .order("user_stylists_count DESC")
+      .limit(5)
+    )}
+
+
+    # scope :stars_more_than, ->(amount) {
+    #   select("stylists.name, stylists.id, average(user_stylists.stars) as stylists_average_rating")
+    #   .joins(:user_stylists)
+    #   where("stylists_average_rating> ? ", amount)
+    #   .group("stylists.id")
+    #   .order("stylists_average_rating DESC")
+    #   .limit(5)
+    # }
+
+    # joins(:user_stylists)
+    # .group("stylists.id")
+
+    # scope :high_rated, -> {(
+      # select("stylists.name, stylists.id, count(user_stylists.stars) as user_stylists_stars")
+      # .joins(:user_stylists)
+      # .group("stylists.id")
+      # .order("user_stylists_stars DEC")
+      # .limit(5)
+      # )}
+
+      # scope :stars_more_than, ->(amount) { where("price > ?", amount) }
 
 end
--- app/models/user_stylist.rb
+++ app/models/user_stylist.rb
@@ -2,8 +2,21 @@ class UserStylist < ApplicationRecord
   belongs_to :user
   belongs_to :stylist
 
-  # accepts_nested_attributes_for :stylist
 
+  validates_presence_of :haircut_date, :stars, :comment, :services, :price
+
+  # accepts_nested_attributes_for :stylist
+  accepts_nested_attributes_for :stylist
+
+  scope :stars_more_than, ->(amount) { where('stars > ?', amount)
+                                      .order(haircut_date: :desc)
+                                      .limit(5)
+                                    }
+    
+
+
+
+  # products.sort_by { |product| product.created_at }.reverse
 
 
 
