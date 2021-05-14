ac6139edf33b3baa37a5150460968a22fcbe6d96
Wonjin Cho <cwonjin11@gmail.com>
2021-05-14 00:46:49 -0400

    root_path, omniauth_path, signup_path re-setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,54 @@
 Rails.application.routes.draw do
+
+
+root "sessions#welcome"
+####### OmniAuth #################   google ########
+# get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
+get '/auth/:provider/callback', to: 'sessions#omniauth'  #<= makes more dynamic
+#################################################################
+
+
+# 🚧 # THINGS WE HAVE TO BUILD OUT
+# get 'welcome', to: "sessions#welcome" 
+# 🚧 #
+######## 
+# REMEMBER FOLKS  ::  NO resource :sessions 
+#################################################################
+
+
+########  SESSIONS ROUTES  ######## SIGN UP IS IN USER ROUTES ALREADY!! 
+get '/signup' => 'users#new'
+post '/signup' => 'users#create'
+
+get 'login', to: "sessions#new"
+post 'login', to: "sessions#create"
+delete 'logout', to: "sessions#destroy"
+#################################################################
+
+
+
+
+#################################################################
+  resources :users do
+
+    resources :user_stylists
+    # resources :stylists
+  end
+
+  resources :user_stylists
+  resources :stylists
+#################################################################
+
+
+
+
+end
+
+
+
+
+
+
   # get 'stylists/index'
   # get 'stylists/show'
   # get 'stylists/new'
@@ -11,44 +61,3 @@ Rails.application.routes.draw do
   # get 'sessions/login'
   # get 'sessions/destroy'
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-####### OmniAuth #########
-
-# get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
-get '/auth/:provider/callback', to: 'sessions#omniauth'
-
-
-########   SESSIONS ROUTES  ### SIGN UP IS IN USER ROUTES ALREADY!! 
-
-get 'login', to: "sessions#new"
-
-post 'login', to: "sessions#create"
-
-delete 'logout', to: "sessions#destroy"
-
-
-# 🚧 # THINGS WE HAVE TO BUILD OUT
-
-get 'welcome', to: "sessions#welcome" 
-
-# 🚧 #
-
-######## 
-# REMEMBER FOLKS  ::  NO resource :sessions 
-
-
-
-  resources :users do
-
-    resources :user_stylists
-    # resources :stylists
-  end
-
-  resources :user_stylists
-  resources :stylists
-
-
-
-
-
-end
