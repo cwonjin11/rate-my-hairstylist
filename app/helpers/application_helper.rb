6aaf4cd55a50bb174ec3a8c1d53036b3169357ac
Wonjin Cho <cwonjin11@gmail.com>
2021-05-14 00:37:19 -0400

    pluralize_without_count helper appended

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+ #### for O review ####
+      def pluralize_without_count(count, noun, text = nil)
+        if count != 0 
+            count == 1 ? "#{noun}#{text}" : "#{noun.pluralize}#{text}"
+        end
+      end
+
+     # <%= pluralize_without_count(@stylist.user_stylists.count, 'review', ':') %>
+
 end
