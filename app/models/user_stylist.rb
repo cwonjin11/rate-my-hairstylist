b8068266a28588c376fa7310523e6ddc4782eccd
Wonjin Cho <cwonjin11@gmail.com>
2021-05-16 01:50:36 -0400

    reinforce price attributes validation

--- app/models/user_stylist.rb
+++ app/models/user_stylist.rb
@@ -1,23 +1,20 @@
 class UserStylist < ApplicationRecord
   belongs_to :user
   belongs_to :stylist
+  # belongs_to :stylist, optional: true
 
 
   validates_presence_of :haircut_date, :stars, :comment, :services, :price
-
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
+  
+  # accepts_nested_attributes_for :stylist, :reject_if => proc { |attributes| attributes['name, phone, shop_name, address'].blank? }
   # accepts_nested_attributes_for :stylist
-  accepts_nested_attributes_for :stylist
 
+
+  ######### argument added in the controller for 5 stars! ################
   scope :stars_more_than, ->(amount) { where('stars > ?', amount)
-                                      .order(haircut_date: :desc)
-                                      .limit(5)
-                                    }
-    
+                                        .order(haircut_date: :desc)
+                                        .limit(5)
+                                      }
 
-
-
-  # products.sort_by { |product| product.created_at }.reverse
-
-
-
-end
+  end
