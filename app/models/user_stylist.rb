93fa15511c130b7692f4d50ed6e4df3c6cd8f4d6
Wonjin Cho <cwonjin11@gmail.com>
2021-05-16 23:36:09 -0400

    price, comment validation reinforced. Add length min/max

--- app/models/user_stylist.rb
+++ app/models/user_stylist.rb
@@ -4,9 +4,12 @@ class UserStylist < ApplicationRecord
   # belongs_to :stylist, optional: true
 
 
-  validates_presence_of :haircut_date, :stars, :comment, :services, :price
-  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
-  
+  validates_presence_of :haircut_date, :stars, :price, :services, :comment
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 2000 }
+  validates :comment, length: { minimum: 2, maximum: 500,
+    too_short: "%{count} characters is the minimum allowed", 
+    too_long: "%{count} characters is the maximum allowed" }
+
   # accepts_nested_attributes_for :stylist, :reject_if => proc { |attributes| attributes['name, phone, shop_name, address'].blank? }
   # accepts_nested_attributes_for :stylist
 
